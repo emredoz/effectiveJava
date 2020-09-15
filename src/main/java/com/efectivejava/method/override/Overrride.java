1b02a94afacbdd92487ffb805eddd8fabf457547
emoz <emoz@ebay.com>
2020-09-15 21:29:49 +0300

    SingletonImpl_v1 ekleme

---
+++ src/main/java/com/efectivejava/method/override/Overrride.java
@@ -0,0 +1,32 @@
+package com.efectivejava.method.override;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Arac{
+    static String getMarka(){
+        return "Arac";
+    }
+}
+
+class Otomobil extends Arac{
+    static String getMarka(){
+        return "Otomobil";
+    }
+}
+
+class Mercedes extends Otomobil{
+    static String getMarka(){
+        return "Mercedes";
+    }
+}
+public class Overrride {
+
+    public static void main(String[] args) {
+        List<Arac> aracList = Arrays.asList(new Arac(),new Otomobil(),new Mercedes()) ;
+
+        aracList.forEach(a->{
+            System.out.println(a.getMarka());
+        });
+    }
+}
